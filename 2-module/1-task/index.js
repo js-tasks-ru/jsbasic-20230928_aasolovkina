4e675baa0f5313ac671f2d9bf38299fe104aa4a1
Anna Solovkina <aasolovkina@gmail.com>
2023-10-06 12:53:26 +0300

    Выполнила задачу номер один из второй лекции

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,23 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalary(salaries) {
   // ваш код...
-}
+  let sum = 0;
+
+  for (key in salaries) {
+    let val = salaries[key];
+
+    if (typeof val == 'number' && isFinite(val)) {
+      sum = sum + val;
+    };
+  };
+ 
+  return sum;
+};
