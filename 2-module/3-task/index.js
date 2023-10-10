17546c92d6f3f83da1d6feb2ffbd53456480a871
Anna Solovkina <aasolovkina@gmail.com>
2023-10-10 10:15:04 +0300

    Выполнила задачу номер три из второй лекции

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
   // ваш код
+  a: 0,
+  b: 0,
+  read: function(a, b) {
+    this.a = a || 0;
+    this.b = b || 0;
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
