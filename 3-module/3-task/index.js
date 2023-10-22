3e017ce655c077e54f6dbe25b84b2c15d548e37d
Anna Solovkina <aasolovkina@gmail.com>
2023-10-22 19:56:59 +0300

    Выполнила задачу номер три из 3 модуля

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
   // ваш код...
+  let arr = str.split('-');
+
+  let resultParts = arr.map(function(word, index) {
+    if (index == 0) {
+      return word;
+    } else {
+      return word[0].toUpperCase() + word.slice(1)
+    }
+  });
+
+  let result = resultParts.join('');
+
+  return result;
 }
